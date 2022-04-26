1c37f05efe9e3bde938f004de19103de63914b4b
Andrexaame <53450386+Andrexaame@users.noreply.github.com>
2022-04-26 11:15:44 -0500

    arreglo paises

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//rutas de paises 
+Route::get('paises',function(){
+    $paises=[
+    "colombia" =>["capital" => "Bogota",
+                 "moneda" => "peso",
+                 "poblacion" => 51.6,
+                 "ciudades" => [
+                    "Medellin",  
+                    "Cali",
+                    "Barranquilla"
+    ]],
+     "Peru" => ["capital" => "Lima",
+                "moneda" => "sol",
+                "poblacion" =>32.97, 
+                "ciudades" => [
+                    "Cusco",  
+                    "Arequipa",
+                    "Trujillo"
+    ]], 
+     "Paraguay" =>["capital" => "Asunsion",
+                  "moneda" => "guanani paraguayo",
+                  "poblacion" => 7.13, 
+                 "ciudades" => [
+                    "Encamacion",  
+                    "Paraguari",
+                    "villarica"
+    ]]
+];
+
+//mostrar la vista de paises
+return view ('paises')->with("paises", $paises);
+
+
+
+
+});
